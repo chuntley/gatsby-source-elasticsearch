2463e9fbb1859e4bcc4ccf500f147428ed71041e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-11-25 01:22:20 +0100

    Fix all auto-fixable linting issues

--- src/query.test.js
+++ src/query.test.js
@@ -1,34 +1,34 @@
-import assert from 'assert'
-import query from './query'
+import assert from 'assert';
+import query from './query';
 
 describe('Query Builder', () => {
   it('builds an empty query', () => {
     const options = {
       index: 'testIndex',
-    }
+    };
 
-    assert.deepEqual(query(options), { index: 'testIndex', scroll: '30s', size: 1000 })
+    assert.deepEqual(query(options), { index: 'testIndex', scroll: '30s', size: 1000 });
   });
 
   it('builds a string query', () => {
     const options = {
       index: 'testIndex',
-      query: 'test query'
-    }
+      query: 'test query',
+    };
 
     assert.deepEqual(query(options), {
       index: 'testIndex',
       scroll: '30s',
       size: 1000,
       q: 'test query',
-    })
+    });
   });
 
   it('builds an object query', () => {
     const options = {
       index: 'testIndex',
       query: { test: 'query' },
-    }
+    };
 
     assert.deepEqual(query(options), {
       index: 'testIndex',
@@ -36,17 +36,17 @@ describe('Query Builder', () => {
       size: 1000,
       body: {
         query: {
-          test: "query"
-        }
-      }
-    })
+          test: 'query',
+        },
+      },
+    });
   });
 
   it('builds an object query body', () => {
     const options = {
       index: 'testIndex',
       body: { query: { test: 'query' } },
-    }
+    };
 
     assert.deepEqual(query(options), {
       index: 'testIndex',
@@ -54,27 +54,27 @@ describe('Query Builder', () => {
       size: 1000,
       body: {
         query: {
-          test: "query"
-        }
-      }
-    })
+          test: 'query',
+        },
+      },
+    });
   });
 
   it('sets the scroll size', () => {
     const options = {
       index: 'testIndex',
       scrollSize: 555,
-    }
+    };
 
-    assert.deepEqual(query(options), { index: 'testIndex', scroll: '30s', size: 555 })
+    assert.deepEqual(query(options), { index: 'testIndex', scroll: '30s', size: 555 });
   });
 
   it('sets the scroll duration', () => {
     const options = {
       index: 'testIndex',
       scrollDuration: '1m',
-    }
+    };
 
-    assert.deepEqual(query(options), { index: 'testIndex', scroll: '1m', size: 1000 })
+    assert.deepEqual(query(options), { index: 'testIndex', scroll: '1m', size: 1000 });
   });
 });
