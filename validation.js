c1baa10ffb6058224780692c7d0fee7e1b276520
mab <git@ma-b.com>
2018-11-25 19:59:14 +0100

    Use helper functions for type tests to improve readability

--- validation.js
+++ validation.js
@@ -4,28 +4,34 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = validation;
+const isString = data => typeof data === 'string';
+
+const isEmptyString = data => data === '';
+
+const isObject = data => typeof data === 'object';
+
 function validation(options) {
-  if (options.typeName == null || typeof options.typeName !== 'string' || options.typeName === '') {
+  if (options.typeName == null || !isString(options.typeName) || isEmptyString(options.typeName)) {
     console.log('Error: "typeName" option is required');
     return false;
   }
 
-  if (options.connection == null || typeof options.connection === 'string' && options.connection === '' && typeof options.connection !== 'object') {
+  if (options.connection == null || isString(options.connection) && isEmptyString(options.connection) && !isObject(options.connection)) {
     console.log('Error: "connection" option must either be a non-empty string or an object');
     return false;
   }
 
-  if (options.index == null || typeof options.index !== 'string' || options.index === '') {
+  if (options.index == null || !isString(options.index) || isEmptyString(options.index)) {
     console.log('Error: "index" option is required');
     return false;
   }
 
-  if (options.query == null || typeof options.query !== 'string' && typeof options.query !== 'object') {
+  if (options.query == null || !isString(options.query) && !isObject(options.query)) {
     console.log('Error: "query" must either be a string or an object');
     return false;
   }
 
-  if (options.scrollDuration && typeof options.scrollDuration !== 'string') {
+  if (options.scrollDuration && !isString(options.scrollDuration)) {
     console.log('Error: "scrollDuration" must be a duration string (i.e. 1s, 10s, 1m)');
     return false;
   }
